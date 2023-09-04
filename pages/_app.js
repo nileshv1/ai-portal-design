5d6fe2e785d2cd59174681c97191289643fe7a19
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-09-04 15:22:49 +0530

    backgrond white line removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import Layout from "@/layout";
+import Head from 'next/head';
 
 function App({ Component, pageProps}) {
   return (
     <Layout>
+      <Head>
+        <body style={{margin:0}} />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/info/info.js
+++ pages/info/info.js
@@ -10,9 +10,21 @@ import { nn_logo, img1, img2, img3, img4 } from "@/public/images";
 import { useEffect, useState } from "react";
 // import styles from "../../styles/style.module.css";
 import { useRouter } from "next/router";
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        border: 'none', // Remove the outline when the TextField is focused
+        // outline: 'none'
+      },
+    },
+  },
+}));
 
 const Info = () => {
-
+    const classes = useStyles();
     const themeinfo = createTheme({
         typography: {
             subtitle2: {
@@ -57,7 +69,7 @@ const Info = () => {
         <ThemeProvider theme={themeinfo}>
             <Grid >
             <BackgroundImage props="true" />
-            <Box sx={{ mx: "auto", width: { xs: "90%", md: "100%" } ,overflow: 'auto' , height:'100vh'}} border="2px solid green" 
+            <Box sx={{ mx: "auto", width: { xs: "90%", md: "100%" } ,overflow: 'auto' , height:'100vh'}} border="0px solid green" 
              style={{position: "absolute",
                 left: 0,
                 right: 0,
@@ -195,7 +207,7 @@ const Info = () => {
                         </Grid>
 
                         {/* //type your question */}
-                        <Grid container item md={11} sx={{ borderRadius: '24px',mb:2, pt: 2, pl: { xs: 2, md: 5 }, pb: 1, pr: 3 }} className={stylesInfo.whiteBg} border="2px solid blue">
+                        <Grid container item md={11} sx={{ borderRadius: '24px',mb:2, pt: 2, pl: { xs: 2, md: 5 }, pb: 1, pr: 3 }} className={stylesInfo.whiteBg} border="0px solid blue">
                             <Box sx={{ display: "flex", justifyContent: "space-between", flexDirection: "column", width: { xs: "100%" } }} border="0px solid red">
                                <Box sx={{ display: "flex", flexDirection: "row" }}>
                                    <TextField
@@ -204,6 +216,7 @@ const Info = () => {
                                         fullWidth
                                         rows={1}
                                         sx={{ outline:"none", border: 'none'}}
+                                        className={classes.root}
                                         InputProps={{
                                             startAdornment: <InputAdornment position="start"><Image
                                             src="/images/search.svg"
