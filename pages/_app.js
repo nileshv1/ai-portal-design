99cda9c92d2da122f687ded54405be12ab27a89a
navnath <navnatha@hexaware.com>
2023-09-05 20:04:03 +0530

    Search API Integrastion

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 // import Layout from "@/layout";
 import dynamic from 'next/dynamic';
 import '../index.css'
+import { Provider } from 'react-redux';
+import store from '@/redux/store'
 
 const Layout = dynamic(() => import("../layout/index.js"), { ssr: false });
 
 function App({ Component, pageProps}) {
   return ( 
+    <Provider store={store}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </Provider>
     
   );
 }
